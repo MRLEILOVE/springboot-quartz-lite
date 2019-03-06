10305223086e9ae703c7a7e51cd2cce88874d451
leigq <191789784@qq.com>
2019-03-06 17:08:38 +0800

    将Quartz的连接池改为Druid

---
+++ src/main/java/com/leigq/quartz/job/BaseJob.java
@@ -0,0 +1,21 @@
+package com.leigq.quartz.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Job基础接口，其他Job实现此接口
+ * <p>
+ * 创建人：LeiGQ <br>
+ * 创建时间：2019-03-06 16:02 <br>
+ * <p>
+ * 修改人： <br>
+ * 修改时间： <br>
+ * 修改备注： <br>
+ * </p>
+ */
+public interface BaseJob extends Job {
+    @Override
+    void execute(JobExecutionContext context) throws JobExecutionException;
+}
