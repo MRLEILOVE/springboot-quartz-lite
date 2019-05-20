df3eac9beb6c08c9ee7059c8b2460a218962a7bd
leigq <191789784@qq.com>
2019-05-20 16:33:56 +0800

    优化代码

--- src/main/java/com/leigq/quartz/controller/JobController.java
+++ src/main/java/com/leigq/quartz/controller/JobController.java
@@ -1,17 +1,19 @@
 package com.leigq.quartz.controller;
 
 import com.github.pagehelper.PageInfo;
+import com.leigq.quartz.bean.Response;
 import com.leigq.quartz.domain.entity.JobAndTrigger;
 import com.leigq.quartz.job.BaseJob;
 import com.leigq.quartz.service.JobAndTriggerService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Objects;
 
 /**
  * Job Controller
@@ -30,11 +32,13 @@ public class JobController {
 
     private final JobAndTriggerService jobAndTriggerService;
     private final Scheduler scheduler;
+    private final Response response;
 
     @Autowired
-    public JobController(JobAndTriggerService jobAndTriggerService, Scheduler scheduler) {
+    public JobController(JobAndTriggerService jobAndTriggerService, Scheduler scheduler, Response response) {
         this.jobAndTriggerService = jobAndTriggerService;
         this.scheduler = scheduler;
+        this.response = response;
     }
 
 
@@ -50,13 +54,39 @@ public class JobController {
      * </p>
      */
     @PostMapping("/jobs")
-    public void addJob(String jobClassName, String jobGroupName, String cronExpression, String jobDescription) throws Exception {
-        // 利用反射获取任务实例，因为所有任务都是实现BaseJob的接口，所以这里使用BaseJob接收
-        BaseJob baseJob = (BaseJob) Class.forName(jobClassName).newInstance();
+    public Response addJob(String jobClassName, String jobGroupName, String cronExpression, String jobDescription) {
+        if (StringUtils.isBlank(jobClassName)) {
+            return response.failure("全类名不能为空！");
+        }
+
+        if (StringUtils.isBlank(jobGroupName)) {
+            return response.failure("任务分组不能为空！");
+        }
+
+        if (StringUtils.isBlank(cronExpression)) {
+            return response.failure("表达式不能为空！");
+        }
+
+        if (StringUtils.isBlank(jobDescription)) {
+            return response.failure("任务描述不能为空！");
+        }
+
+        // 验证表达式格式
+        if (!CronExpression.isValidExpression(cronExpression)) {
+            return response.failure("表达式格式错误！");
+        }
+
+        BaseJob baseJob;
+        try {
+            // 利用反射获取任务实例，因为所有任务都是实现BaseJob的接口，所以这里使用BaseJob接收
+            baseJob = (BaseJob) Class.forName(jobClassName).newInstance();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            return response.failure("全类名错误！");
+        }
 
         //构建job信息
         JobDetail jobDetail = JobBuilder.newJob(baseJob.getClass())
-                .withIdentity(jobClassName, jobGroupName)
+                .withIdentity(baseJob.getClass().getSimpleName(), jobGroupName)
                 .withDescription(jobDescription)
                 .build();
 
@@ -75,9 +105,10 @@ public class JobController {
             scheduler.scheduleJob(jobDetail, trigger);
             // 启动调度器
             scheduler.start();
+            return response.success("创建定时任务成功！");
         } catch (SchedulerException e) {
             log.error("创建定时任务失败:", e);
-            throw new Exception("创建定时任务失败");
+            return response.failure("创建定时任务失败！");
         }
     }
 
@@ -94,8 +125,14 @@ public class JobController {
      * </p>
      */
     @PostMapping("/jobs/action/pause")
-    public void pauseJob(String jobClassName, String jobGroupName) throws Exception {
-        scheduler.pauseJob(JobKey.jobKey(jobClassName, jobGroupName));
+    public Response pauseJob(String jobClassName, String jobGroupName) {
+        try {
+            scheduler.pauseJob(JobKey.jobKey(jobClassName, jobGroupName));
+            return response.success("暂停任务成功！");
+        } catch (SchedulerException e) {
+            log.error("暂停任务异常", e);
+            return response.failure("暂停任务失败！");
+        }
     }
 
     /**
@@ -110,8 +147,15 @@ public class JobController {
      * </p>
      */
     @PostMapping("/jobs/action/resume")
-    public void resumeJob(String jobClassName, String jobGroupName) throws Exception {
-        scheduler.resumeJob(JobKey.jobKey(jobClassName, jobGroupName));
+    public Response resumeJob(String jobClassName, String jobGroupName) {
+        try {
+            scheduler.resumeJob(JobKey.jobKey(jobClassName, jobGroupName));
+            return response.success("恢复任务成功！");
+        } catch (SchedulerException e) {
+            log.error("恢复任务异常", e);
+            return response.failure("恢复任务失败！");
+        }
+
     }
 
 
@@ -127,9 +171,17 @@ public class JobController {
      * </p>
      */
     @PutMapping("/jobs")
-    public void rescheduleJob(String jobClassName, String jobGroupName, String cronExpression) throws Exception {
+    public Response rescheduleJob(String jobClassName, String jobGroupName, String cronExpression) {
+        if (StringUtils.isBlank(cronExpression)) {
+            return response.failure("表达式不能为空！");
+        }
+        // 验证表达式格式
+        if (!CronExpression.isValidExpression(cronExpression)) {
+            return response.failure("表达式格式错误！");
+        }
         try {
             TriggerKey triggerKey = TriggerKey.triggerKey(jobClassName, jobGroupName);
+
             // 表达式调度构建器
             CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(cronExpression);
 
@@ -140,9 +192,10 @@ public class JobController {
 
             // 按新的trigger重新设置job执行
             scheduler.rescheduleJob(triggerKey, trigger);
+            return response.success("更新任务成功！");
         } catch (SchedulerException e) {
-            log.error("更新定时任务失败", e);
-            throw new Exception("更新定时任务失败");
+            log.error("更新任务失败", e);
+            return response.failure("更新任务失败！");
         }
     }
 
@@ -158,10 +211,17 @@ public class JobController {
      * </p>
      */
     @DeleteMapping("/jobs")
-    public void deleteJob(String jobClassName, String jobGroupName) throws Exception {
-        scheduler.pauseTrigger(TriggerKey.triggerKey(jobClassName, jobGroupName));
-        scheduler.unscheduleJob(TriggerKey.triggerKey(jobClassName, jobGroupName));
-        scheduler.deleteJob(JobKey.jobKey(jobClassName, jobGroupName));
+    public Response deleteJob(String jobClassName, String jobGroupName) {
+        try {
+            scheduler.pauseTrigger(TriggerKey.triggerKey(jobClassName, jobGroupName));
+            scheduler.unscheduleJob(TriggerKey.triggerKey(jobClassName, jobGroupName));
+            scheduler.deleteJob(JobKey.jobKey(jobClassName, jobGroupName));
+            return response.success("删除任务成功！");
+        } catch (SchedulerException e) {
+            log.error("删除任务异常", e);
+            return response.failure("删除任务失败！");
+        }
+
     }
 
     /**
@@ -176,12 +236,12 @@ public class JobController {
      * </p>
      */
     @GetMapping("/jobs")
-    public Map<String, Object> queryJob(Integer pageNum, Integer pageSize) {
+    public Response queryJob(Integer pageNum, Integer pageSize) {
         PageInfo<JobAndTrigger> jobAndTrigger = jobAndTriggerService.getJobAndTriggerDetails(pageNum, pageSize);
         Map<String, Object> map = new HashMap<>();
         map.put("JobAndTrigger", jobAndTrigger);
         map.put("number", jobAndTrigger.getTotal());
-        return map;
+        return response.success(map);
     }
 
 }
