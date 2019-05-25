a2d9a3506f65e11a04b255beb1b6a055b445c266
leigq <191789784@qq.com>
2019-05-25 11:16:10 +0800

    优化页面

--- src/main/java/com/leigq/quartz/config/WebMvcConfig.java
+++ src/main/java/com/leigq/quartz/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.leigq.quartz.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HttpPutFormContentFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -28,5 +30,19 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("job-manager.html")
                 .addResourceLocations("classpath:/templates/");
+    }
+
+    /**
+     * 解决SpringBoot PUT请求不能接收到参数的问题
+     * <br/>
+     * 参考：https://www.cnblogs.com/aliyunpang/p/8962694.html
+     * 开启：HttpPutFormContentFilter
+     * @author ：LeiGQ <br>
+     * @date ：2019-05-25 11:01 <br>
+     * <p>
+     */
+    @Bean
+    public HttpPutFormContentFilter httpPutFormContentFilter() {
+        return new HttpPutFormContentFilter();
     }
 }
