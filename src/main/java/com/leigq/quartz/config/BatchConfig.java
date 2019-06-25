d70c6ec521652c8de9926a4a4936e519ebd6db77
leigq <191789784@qq.com>
2019-06-25 16:34:11 +0800

    添加 SpringBatch 依赖

---
+++ src/main/java/com/leigq/quartz/config/BatchConfig.java
@@ -0,0 +1,17 @@
+package com.leigq.quartz.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：leigq
+ * @date ：2019/6/25 10:26
+ * @description：SpringBatch配置
+ */
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+
+
+}
