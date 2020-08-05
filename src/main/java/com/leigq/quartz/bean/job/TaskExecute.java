011bc6e297cdf75e9920648cebb9df6c0a5121e1
leigq <191789784@qq.com>
2020-08-05 15:49:53 +0800

    新增任务执行的抽象类（模板），自己的任务继承此类，重写 execute 方法来执行自己的任务

---
+++ src/main/java/com/leigq/quartz/bean/job/TaskExecute.java
@@ -0,0 +1,82 @@
+package com.leigq.quartz.bean.job;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.leigq.quartz.bean.dto.AddQuartzJobDTO;
+import com.leigq.quartz.bean.enumeration.SysTaskExecResultEnum;
+import com.leigq.quartz.domain.entity.SysTask;
+import com.leigq.quartz.domain.entity.SysTaskLog;
+import com.leigq.quartz.service.SysTaskLogService;
+import com.leigq.quartz.service.SysTaskService;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * 任务执行的抽象类（模板），自己的任务继承此类，重写 execute 方法来执行自己的任务
+ */
+@Component
+public abstract class TaskExecute {
+
+    private AddQuartzJobDTO addQuartzJobDTO;
+
+    @Autowired
+    private SysTaskService sysTaskService;
+
+    @Autowired
+    private SysTaskLogService sysTaskLogService;
+
+
+    public void setAddQuartzJobDTO(AddQuartzJobDTO addQuartzJobDTO) {
+        this.addQuartzJobDTO = addQuartzJobDTO;
+    }
+
+    public void execute() {
+        // 添加任务执行日志
+        SysTaskLog sysTaskLog = SysTaskLog.builder()
+                .taskName(addQuartzJobDTO.getTaskName())
+                .execDate(new Date())
+                .execResult(SysTaskExecResultEnum.SUCCESS.getValue())
+                .execResultText("任务执行成功")
+                .taskId(addQuartzJobDTO.getTaskId())
+                .build();
+        sysTaskLogService.save(sysTaskLog);
+
+        // 更新任务的最近一次执行时间、最近一次执行结果字段
+        sysTaskService.update(Wrappers.<SysTask>lambdaUpdate()
+                .set(SysTask::getExecDate, new Date())
+                .set(SysTask::getExecResult, SysTaskExecResultEnum.SUCCESS.getValue())
+                .eq(SysTask::getId, addQuartzJobDTO.getTaskId())
+        );
+        try {
+            // 获取任务携带的参数
+            Map<String, Object> dataMap = addQuartzJobDTO.getDataMap();
+            // 调用子类的任务
+            execute(dataMap);
+        } catch (Exception e) {
+            // 将执行结果改为失败并记录异常信息
+            sysTaskLogService.update(Wrappers.<SysTaskLog>lambdaUpdate()
+                    .set(SysTaskLog::getExecResult, SysTaskExecResultEnum.FAILURE.getValue())
+                    .set(SysTaskLog::getExecResultText, ExceptionUtils.getStackTrace(e))
+                    .eq(SysTaskLog::getId, sysTaskLog.getId())
+            );
+
+            sysTaskService.update(Wrappers.<SysTask>lambdaUpdate()
+                    .set(SysTask::getExecResult, SysTaskExecResultEnum.FAILURE.getValue())
+                    .eq(SysTask::getId, addQuartzJobDTO.getTaskId())
+            );
+            // 回滚事务
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        }
+    }
+
+    /**
+     * 自定义任务子类需实现此方法
+     *
+     * @param dataMap 添加任务时配置的参数，如：aaa=111;bbb=222
+     */
+    public abstract void execute(Map<String, Object> dataMap) throws Exception;
+}
