6085ddae158447c36d9f7b22998b08c84282da6a
leigq <191789784@qq.com>
2020-08-05 15:52:48 +0800

    修改BaseJob，使其成为唯一一个任务，现在每个任务执行，实际上都是在执行BaseJob，然后BaseJob利用反射调用TaskExecute的子类来执行任务

--- src/main/java/com/leigq/quartz/bean/job/BaseJob.java
+++ src/main/java/com/leigq/quartz/bean/job/BaseJob.java
@@ -1,21 +1,38 @@
 package com.leigq.quartz.bean.job;
 
+import com.leigq.quartz.bean.dto.AddQuartzJobDTO;
+import com.leigq.quartz.util.SpringContextHolder;
+import com.leigq.quartz.web.exception.ServiceException;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
 /**
- * Job基础接口，其他Job实现此接口
+ * 任务基类，所有任务都继承此类
  * <p>
  * 创建人：LeiGQ <br>
  * 创建时间：2019-03-06 16:02 <br>
- * <p>
- * 修改人： <br>
- * 修改时间： <br>
- * 修改备注： <br>
- * </p>
  */
-public interface BaseJob extends Job {
+public class BaseJob implements Job {
+
     @Override
-    void execute(JobExecutionContext context) throws JobExecutionException;
+    public void execute(JobExecutionContext context) {
+        // 获取 Job 中定义属性 （调用任务的时候传递进来的参数）
+        JobDataMap data = context.getJobDetail().getJobDataMap();
+
+        // 获取创建任务时传递过来的参数
+        AddQuartzJobDTO addQuartzJobDTO = (AddQuartzJobDTO) data.get("addQuartzJobDTO");
+
+        try {
+            // 获取自定义任务的类
+            Class<?> clazz = Class.forName(addQuartzJobDTO.getTaskClass());
+            // 获取自定义任务实例，自定义任务全部继承 TaskExecute
+            TaskExecute taskExecute = (TaskExecute) SpringContextHolder.getBean(clazz);
+            taskExecute.setAddQuartzJobDTO(addQuartzJobDTO);
+            taskExecute.execute();
+        } catch (Exception e) {
+            String errorMessage = String.format("任务: [%s] 未启动成功，请检查执行类是否配置正确！！！", addQuartzJobDTO.getTaskName());
+            throw new ServiceException(errorMessage, e);
+        }
+    }
 }
