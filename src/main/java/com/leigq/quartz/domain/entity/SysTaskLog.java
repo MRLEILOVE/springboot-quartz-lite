85184d7e48402ed41e619b4bc2384e0e9641fade
leigq <191789784@qq.com>
2020-08-06 18:54:50 +0800

    时间格式处理

--- src/main/java/com/leigq/quartz/domain/entity/SysTaskLog.java
+++ src/main/java/com/leigq/quartz/domain/entity/SysTaskLog.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -36,6 +38,7 @@ public class SysTaskLog implements Serializable {
      * 执行时间
      */
     @TableField(value = "exec_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date execDate;
 
     /**
