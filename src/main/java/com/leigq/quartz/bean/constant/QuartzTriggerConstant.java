59c4435503d18d41825c3d4e07a30a6cf14f0eb9
leigq <191789784@qq.com>
2020-08-06 17:00:12 +0800

    优化代码

--- src/main/java/com/leigq/quartz/bean/constant/QuartzTriggerConstant.java
+++ src/main/java/com/leigq/quartz/bean/constant/QuartzTriggerConstant.java
@@ -8,6 +8,12 @@ public interface QuartzTriggerConstant {
 
     String TRIGGER_NAME_FORMAT = "%sTrigger";
 
+    /**
+     * Trigger name string.
+     *
+     * @param prefix the prefix
+     * @return the string
+     */
     static String triggerName(String prefix) {
         return String.format(TRIGGER_NAME_FORMAT, prefix);
     }
--- src/main/java/com/leigq/quartz/domain/entity/SysTask.java
+++ src/main/java/com/leigq/quartz/domain/entity/SysTask.java
@@ -14,6 +14,7 @@ import lombok.experimental.FieldNameConstants;
 
 /**
  * 任务表
+ * @author leigq
  */
 @Builder
 @AllArgsConstructor
