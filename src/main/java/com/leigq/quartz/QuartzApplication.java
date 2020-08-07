728cba16d58382903040a6ea99891fa0f91fc6ad
leigq <191789784@qq.com>
2020-08-07 14:37:12 +0800

    去除Mongodb自动配置限制

--- src/main/java/com/leigq/quartz/QuartzApplication.java
+++ src/main/java/com/leigq/quartz/QuartzApplication.java
@@ -3,14 +3,13 @@ package com.leigq.quartz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 /**
  * The type Quartz application.
  *
  * @author leigq
  */
-@SpringBootApplication(exclude = MongoAutoConfiguration.class)
+@SpringBootApplication
 @MapperScan("com.leigq.quartz.domain.mapper")
 public class QuartzApplication {
 
